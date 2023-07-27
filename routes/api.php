bc7b906ec4dd88327681ac42a09416d212d65ed0
Juanma <juanm.ramirezs@autonoma.com.co>
2023-07-27 18:33:53 -0500

    Configure nuevas rutas y api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\v1\CategoryController;
+use App\Http\Controllers\api\v1\ProjectController;
+use App\Http\Controllers\api\v1\CommentsController;
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +19,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::apiResource('v1/category', CategoryController::class);
+Route::apiResource('v1/project', ProjectController::class);
+Route::apiResource('v1/comments', CommentsController::class);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
