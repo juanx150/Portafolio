2fc3c86a31ea6a4d8014c51297854064c79be90c
Juanma <juanm.ramirezs@autonoma.com.co>
2023-07-28 07:53:13 -0500

    Mejore el manejo de errores de api

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,9 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Log;
+
 
 class Handler extends ExceptionHandler
 {
@@ -17,10 +20,27 @@ class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
-
+    public function handleApiExceptions($request, $exception)
+    {
+        if($exception instanceof ModelNotFoundException)
+        {
+            return response()->json(['error' => 'Model Not Found'], 404);
+        }
+        Log::warning("[Handler.handleApiExceptions] API Exception type '" . 
+        get_class($exception) . "' not handled.");
+        return parent::render($request, $exception);
+    }
     /**
      * Register the exception handling callbacks for the application.
      */
+    public function render($request, Throwable $exception)
+    {
+        if($request->expectsJson())
+        {
+        return $this->handleApiExceptions($request, $exception);
+        }
+        return parent::render($request, $exception);
+    }
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
